e459b7c58aeb4abe9922d4660de9cb40e93ae29b
TomerLankri <39373833+TomerLankri@users.noreply.github.com>
2024-01-31 08:22:37 +0200

    Add files via upload

---
+++ locofy-react-nextjs-project/pages/index.tsx
@@ -0,0 +1,300 @@
+import type { NextPage } from "next";
+import styles from "./index.module.css";
+
+const Dashboard: NextPage = () => {
+  return (
+    <div className={styles.dashboard}>
+      <div className={styles.bankParent}>
+        <div className={styles.bank}>
+          <div className={styles.addTemplateToProjectWrapper}>
+            <div className={styles.addTemplateTo}>Add Template To Project</div>
+          </div>
+          <div className={styles.editParent}>
+            <img className={styles.editIcon} alt="" src="/edit.svg" />
+            <img className={styles.closeIcon} alt="" src="/close.svg" />
+          </div>
+          <div className={styles.frameParent}>
+            <div className={styles.beginnerWrapper}>
+              <div className={styles.beginner}>Beginner</div>
+            </div>
+            <div className={styles.frameGroup}>
+              <div className={styles.groupParent}>
+                <div className={styles.checkboxBaseParent}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>1 Dashboard</div>
+                </div>
+                <div className={styles.checkboxBaseGroup}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>4 Models</div>
+                </div>
+                <div className={styles.checkboxBaseContainer}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>10 Charts</div>
+                </div>
+                <div className={styles.groupDiv}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>44 Variables</div>
+                </div>
+              </div>
+              <div className={styles.dashboardAnd4SimpleModelsParent}>
+                <div className={styles.dashboardAnd4}>
+                  1 Dashboard and 4 simple models to create common B2C SaaS
+                  monitoring
+                </div>
+                <div className={styles.b2cSaasTemplateParent}>
+                  <b className={styles.b2cSaasTemplate}>B2C SaaS Template</b>
+                  <div className={styles.groupWrapper}>
+                    <div className={styles.saasParent}>
+                      <div className={styles.saas}>SaaS</div>
+                      <img className={styles.tagsIcon} alt="" src="/tags.svg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.bank}>
+          <div className={styles.addTemplateToProjectWrapper}>
+            <div className={styles.addTemplateTo}>Add Template To Project</div>
+          </div>
+          <div className={styles.editParent}>
+            <img className={styles.editIcon} alt="" src="/edit.svg" />
+            <img className={styles.closeIcon} alt="" src="/close.svg" />
+          </div>
+          <div className={styles.frameContainer}>
+            <div className={styles.intermediateWrapper}>
+              <div className={styles.intermediate}>Intermediate</div>
+            </div>
+            <div className={styles.frameParent1}>
+              <div className={styles.groupParent}>
+                <div className={styles.checkboxBaseParent}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>1 Dashboard</div>
+                </div>
+                <div className={styles.checkboxBaseGroup}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>4 Models</div>
+                </div>
+                <div className={styles.checkboxBaseContainer}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>10 Charts</div>
+                </div>
+                <div className={styles.groupDiv}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>44 Variables</div>
+                </div>
+              </div>
+              <div className={styles.dashboardAnd4SimpleModelsParent}>
+                <div className={styles.dashboardAnd4}>
+                  1 Dashboard and 4 simple models to create common B2C SaaS
+                  monitoring
+                </div>
+                <div className={styles.b2cSaasTemplateParent}>
+                  <b className={styles.b2cSaasTemplate}>B2C SaaS Template</b>
+                  <div className={styles.groupWrapper}>
+                    <div className={styles.saasParent}>
+                      <div className={styles.saas}>SaaS</div>
+                      <img className={styles.tagsIcon} alt="" src="/tags.svg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.bank}>
+          <div className={styles.addTemplateToProjectWrapper}>
+            <div className={styles.addTemplateTo}>Add Template To Project</div>
+          </div>
+          <div className={styles.editParent}>
+            <img className={styles.editIcon} alt="" src="/edit.svg" />
+            <img className={styles.closeIcon} alt="" src="/close.svg" />
+          </div>
+          <div className={styles.frameParent}>
+            <div className={styles.beginnerWrapper}>
+              <div className={styles.beginner}>Beginner</div>
+            </div>
+            <div className={styles.frameGroup}>
+              <div className={styles.groupParent}>
+                <div className={styles.checkboxBaseParent}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>1 Dashboard</div>
+                </div>
+                <div className={styles.checkboxBaseGroup}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>4 Models</div>
+                </div>
+                <div className={styles.checkboxBaseContainer}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>10 Charts</div>
+                </div>
+                <div className={styles.groupDiv}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>44 Variables</div>
+                </div>
+              </div>
+              <div className={styles.dashboardAnd4SimpleModelsParent}>
+                <div className={styles.dashboardAnd4}>
+                  1 Dashboard and 4 simple models to create common B2C SaaS
+                  monitoring
+                </div>
+                <div className={styles.b2cSaasTemplateParent}>
+                  <b className={styles.b2cSaasTemplate}>B2C SaaS Template</b>
+                  <div className={styles.groupWrapper}>
+                    <div className={styles.saasParent}>
+                      <div className={styles.saas}>SaaS</div>
+                      <img className={styles.tagsIcon} alt="" src="/tags.svg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.bank}>
+          <div className={styles.addTemplateToProjectWrapper}>
+            <div className={styles.addTemplateTo}>Add Template To Project</div>
+          </div>
+          <div className={styles.editParent}>
+            <img className={styles.editIcon} alt="" src="/edit.svg" />
+            <img className={styles.closeIcon} alt="" src="/close.svg" />
+          </div>
+          <div className={styles.frameContainer}>
+            <div className={styles.intermediateWrapper}>
+              <div className={styles.intermediate}>Intermediate</div>
+            </div>
+            <div className={styles.frameParent1}>
+              <div className={styles.groupParent}>
+                <div className={styles.checkboxBaseParent}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>1 Dashboard</div>
+                </div>
+                <div className={styles.checkboxBaseGroup}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>4 Models</div>
+                </div>
+                <div className={styles.checkboxBaseContainer}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>10 Charts</div>
+                </div>
+                <div className={styles.groupDiv}>
+                  <img
+                    className={styles.checkboxBaseIcon}
+                    alt=""
+                    src="/-checkbox-base.svg"
+                  />
+                  <div className={styles.models}>44 Variables</div>
+                </div>
+              </div>
+              <div className={styles.dashboardAnd4SimpleModelsParent}>
+                <div className={styles.dashboardAnd4}>
+                  1 Dashboard and 4 simple models to create common B2C SaaS
+                  monitoring
+                </div>
+                <div className={styles.b2cSaasTemplateParent}>
+                  <b className={styles.b2cSaasTemplate}>B2C SaaS Template</b>
+                  <div className={styles.groupWrapper}>
+                    <div className={styles.saasParent}>
+                      <div className={styles.saas}>SaaS</div>
+                      <img className={styles.tagsIcon} alt="" src="/tags.svg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.frameParent6}>
+        <div className={styles.frameWrapper}>
+          <div className={styles.allTemplatesWrapper}>
+            <div className={styles.allTemplates}>All Templates</div>
+          </div>
+        </div>
+        <div className={styles.frameWrapper1}>
+          <div className={styles.allTemplatesWrapper}>
+            <div className={styles.allTemplates}>Templates in use</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.iconChevronParent}>
+        <img
+          className={styles.iconChevron}
+          alt=""
+          src="/icon--chevron@2x.png"
+        />
+        <div className={styles.title}>ACME (Demo)</div>
+        <div className={styles.title1}>Template Library</div>
+      </div>
+      <img className={styles.dashboardChild} alt="" src="/line-7.svg" />
+      <div className={styles.dashboardInner}>
+        <div className={styles.searchATemplateWrapper}>
+          <div className={styles.searchATemplate}>Search a Template</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
